da9bf61517f779e05ede20cae8a77ca21730fdbd
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2019-01-05 07:27:56 -0500

    Base Layout done. Fixing white spaces and footer next.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,47 @@
-$(document).ready(function () {
-  var $container = $(".projects-feed"),
-    //   containerWidth = $container.outerWidth();
+$(function () {
 
-    $container.imagesLoaded(function () {
-      $container.masonry({
-        itemSelector: ".project",
-        columnWidth: 290,
-        animate: true,
-        prepend($container)
-      });
+  // Vars.
+  var $window = $(window),
+    $body = $('body'),
+    $wrapper = $('#wrapper');
+
+  // Breakpoints.
+  skel.breakpoints({
+    xlarge: '(max-width: 1680px)',
+    large: '(max-width: 1280px)',
+    medium: '(max-width: 980px)',
+    small: '(max-width: 736px)',
+    xsmall: '(max-width: 480px)'
+  });
+
+  // Disable animations/transitions until everything's loaded.
+  $body.addClass('is-loading');
+
+  $window.on('load', function () {
+    $body.removeClass('is-loading');
+  });
+
+  // Poptrox.
+  $window.on('load', function () {
+
+    $('.thumbnails').poptrox({
+      onPopupClose: function () {
+        $body.removeClass('is-covered');
+      },
+      onPopupOpen: function () {
+        $body.addClass('is-covered');
+      },
+      baseZIndex: 10001,
+      useBodyOverflow: false,
+      usePopupEasyClose: true,
+      overlayColor: '#000000',
+      overlayOpacity: 0.75,
+      popupLoaderText: '',
+      fadeSpeed: 500,
+      usePopupDefaultStyling: false,
+      windowMargin: (skel.breakpoint('small').active ? 5 : 50)
     });
-});
 
-// isAnimated: !Modernizr.csstransitions
+  });
+
+});
