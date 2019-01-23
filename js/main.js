430f7caad7cbf0066060276f8343b4f1a876bbee
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2019-01-23 12:39:30 -0500

    css and html optimized

--- js/main.js
+++ js/main.js
@@ -1,63 +1,36 @@
-$(function () {
-
+$(function() {
   // Vars.
   var $window = $(window),
-    $body = $('body'),
-    $wrapper = $('#wrapper'),
-    $thumbnails = $('.thumbnails');
+    $body = $("body"),
+    $wrapper = $("#wrapper"),
+    $thumbnails = $(".thumbnails");
 
   // Breakpoints.
   skel.breakpoints({
-    xlarge: '(max-width: 1680px)',
-    large: '(max-width: 1280px)',
-    medium: '(max-width: 980px)',
-    small: '(max-width: 736px)',
-    xsmall: '(max-width: 480px)'
+    xlarge: "(max-width: 1680px)",
+    large: "(max-width: 1280px)",
+    medium: "(max-width: 980px)",
+    small: "(max-width: 736px)",
+    xsmall: "(max-width: 480px)"
   });
 
   // Disable animations/transitions until everything's loaded.
-  $body.addClass('is-loading');
+  $body.addClass("is-loading");
 
-  $window.on('load', function () {
-    $body.removeClass('is-loading');
+  $window.on("load", function() {
+    $body.removeClass("is-loading");
   });
 
-  // Poptrox.
-  $window.on('load', function () {
-
-    $thumbnails.poptrox({
-      onPopupOpen: function () {
-        $body.addClass('is-covered');
-      },
-      onPopupClose: function () {
-        $body.removeClass('is-covered');
-      },
-      baseZIndex: 1000,
-      useBodyOverflow: false,
-      usePopupEasyClose: true,
-      overlayColor: '#000000',
-      overlayOpacity: 0.75,
-      popupLoaderText: '',
-      fadeSpeed: 500,
-      usePopupDefaultStyling: false,
-      windowMargin: (skel.breakpoint('small').active ? 5 : 50)
-    });
-
-  });
-
-  $(document).ready(function () {
-
-    $('.cropped-images img').each(function () {
+  $(document).ready(function() {
+    $(".cropped-images img").each(function() {
       if ($(this).width() > $(this).height()) {
-        $(this).addClass('landscape');
+        $(this).addClass("landscape");
       }
     });
-
   });
 
-  var height_diff = $(window).height() - $('body').height();
+  var height_diff = $(window).height() - $("body").height();
   if (height_diff > 0) {
-    $('footer').css('margin-top', height_diff);
+    $("footer").css("margin-top", height_diff);
   }
-
 });
